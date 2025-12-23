63b3c14cd83581713bd5b269f80a5540c275590f
alisabet <alisabetiweb@gmail.com>
2025-12-23 13:22:06 +0330

    project init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-red-200'>Lorem ipsum dolor sit amet.</h1>
+      </div>
+    </>
+  )
+}
+
+export default App

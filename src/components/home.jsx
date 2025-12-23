a87e36cb00a07f264da0e1f6b6a6854f75ec75d4
alisabet <alisabetiweb@gmail.com>
2025-12-23 18:03:25 +0330

    home page gradient added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import GradientBlinds from './GradientBlinds '
 
 function Home() {
   return (
-    <div className='text-3xl'>Home</div>
+    <div className='h-svh w-full relative'>
+        <GradientBlinds
+            gradientColors={['#eb8615', '#903f05']}
+            angle={30}
+            noise={0.1}
+            blindCount={12}
+            blindMinWidth={50}
+            spotlightRadius={0.5}
+            spotlightSoftness={1}
+            spotlightOpacity={1}
+            mouseDampening={0.5}
+            distortAmount={0}
+            shineDirection="left"
+            mixBlendMode="lighten"
+        />
+    </div>
   )
 }
 

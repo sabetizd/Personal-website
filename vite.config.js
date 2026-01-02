b978294c65ed50a4f3359ad754790983361d2f70
alisabet <alisabetiweb@gmail.com>
2026-01-02 15:29:16 +0330

    tailwind tree installed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { twTreePlugin } from '@tailwind-tree/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    twTreePlugin(),
   ],
   resolve:{
     alias:{
